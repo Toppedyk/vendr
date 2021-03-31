79fceff7a0bd45d233987b6fc7d635ed7ae78821
Toppedyk <Tayloroppedyk@yahoo.com>
2021-03-30 20:02:12 -0600

    streamlined functions

--- app/Controllers/MoneyController.js
+++ app/Controllers/MoneyController.js
@@ -5,25 +5,22 @@ import {moneyService} from '../Services/MoneyService.js'
 function _draw(){
 let money = ProxyState.totalMoney;
 document.getElementById('total').innerHTML = `Total money: $${money}`
+}
 
-document.getElementById('count').innerHTML=
-`<div class="row">
-<div class="col" id='count'>
-    <h4>Snacks Purchased:</h4>
-    <ul>
-        <li>Cheetos: ${ProxyState.snacks[0].owned}</li>
-        <li>Milky Way: ${ProxyState.snacks[1].owned}</li>
-        <li>Oreos: ${ProxyState.snacks[2].owned}</li>
-    </ul>
-</div>
-</div>`
+function _drawPurchase(){
+  let template=''
+  
+  ProxyState.snacks.forEach((snack,i)=>{
+    template+=`<li>${ProxyState.snacks[i].name}: ${ProxyState.snacks[i].owned}</li>`
+  })
+  document.getElementById('count').innerHTML=`${template}`
 }
 
 function _drawButton(){
   let template=''
   
   ProxyState.snacks.forEach((snack,i) => {
-    template += `<button class="btn btn-warning" onclick="app.moneyController.buySnack(${i})">Buy ${snack.name}: $${snack.price}</button>`
+    template += `<button class="btn btn-warning m-2" onclick="app.moneyController.buySnack(${i})">Buy ${snack.name}: $${snack.price}</button>`
   })
   document.getElementById('buttons').innerHTML=`${template}`;
 }
@@ -33,7 +30,9 @@ export default class MoneyController{
   constructor(){
     console.log('Money Controller')
     ProxyState.on('totalMoney',_draw)
+    ProxyState.on('totalMoney',_drawPurchase)
     _drawButton()
+    _drawPurchase()
   }
   addQuarter(){
     moneyService.addQuarter();
